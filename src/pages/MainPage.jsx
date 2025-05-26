901b7a9ae7955201a49bf82910fec287d95427e3
huntercordes <huntercordes@gmail.com>
2025-05-26 10:55:40 +0200

    final changes

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import InfoImageSection from '../components/InfoImageSection';
 import outsideImage from '../assets/outsidemuseum.png';
-import fox from '../assets/fox.png';
+import placeholder from '../assets/placeholder.png'; 
 import InfoCardsRow from '../components/InfoCardsRow';
 import EventsRow from '../components/EventsRow';
 import ReviewRow from '../components/ReviewRow';
@@ -24,18 +24,11 @@ const MainPage = () => {
         <InfoCardsRow />
         <EventsRow />
       <InfoImageSection
-    imageSrc={fox}
-        title="Discover Prehistoric Life"
-        text="Uncover fossils and stories from millions of years ago."
-        links={[{ label: 'Explore Fossils', href: '/fossils' }]}
+    imageSrc={placeholder}
+        title="Explore Our Digital Journal"
+        text="Stay informed with the latest research, discoveries, and natural science insights from the Naturhistorisk Museum. Dive into peer reviewed articles, fieldwork reports, and expert contributions all in one accessible digital platform."
+        links={[{ label: 'Learn More', href: '/animal-exhibition/mobile' }]}
         imageOnLeft={true}
-      />
-      <InfoImageSection
-    imageSrc={fox}
-        title="Discover Prehistoric Life"
-        text="Uncover fossils and stories from millions of years ago."
-        links={[{ label: 'Explore Fossils', href: '/fossils' }]}
-        imageOnLeft={false}
       />
       <ReviewRow />
     </main>
